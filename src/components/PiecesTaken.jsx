be3543c12a873a3b05c9380a910b82f57f29dfcc
Emerson Lima <emersonlimago123@gmail.com>
2024-03-13 13:25:49 -0300

    added surplus of pieces taken

--- src/components/PiecesTaken.jsx
+++ src/components/PiecesTaken.jsx
@@ -2,9 +2,38 @@ import classes from "./PiecesTaken.module.css";
 
 export default function PiecesTaken({ piecesTaken, color }) {
   let identifier = color == "white" ? "w" : "b";
-  console.log(piecesTaken);
+
+  let extraValue = 0;
+  let oponentValue = 0;
+  let oponentColor = color == "white" ? "black" : "white";
+  let oponentIdentifier = color == "white" ? "b" : "w";
+
+  piecesTaken[color].forEach((piece) => {
+    if (piece.includes("p")) extraValue++;
+    if (piece.includes("n") || piece.includes(`${identifier + "b"}`))
+      extraValue += 3;
+    if (piece.includes("h")) extraValue += 5;
+    if (piece.includes("q")) extraValue += 9;
+  });
+
+  piecesTaken[oponentColor].forEach((piece) => {
+    if (piece.includes("p")) oponentValue++;
+    if (piece.includes("n") || piece.includes(`${oponentIdentifier + "b"}`))
+      oponentValue += 3;
+    if (piece.includes("h")) oponentValue += 5;
+    if (piece.includes("q")) oponentValue += 9;
+  });
+
+  extraValue = extraValue - oponentValue;
+
   return (
-    <div className={classes["pieces-taken"]}>
+    <div
+      className={
+        extraValue >= 1
+          ? `${classes["pieces-taken"]} ${classes["pieces-taken-extra"]}`
+          : `${classes["pieces-taken"]}`
+      }
+    >
       <div>
         {piecesTaken[color].map((piece, index) => {
           if (typeof piece == "string" && piece.includes("p")) {
@@ -43,6 +72,7 @@ export default function PiecesTaken({ piecesTaken, color }) {
           }
         })}
       </div>
+      {extraValue >= 1 && <p>{"+" + extraValue}</p>}
     </div>
   );
 }
